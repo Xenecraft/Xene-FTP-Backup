cb4aa11001b5286e59532f062d7e847117a61be0
Eric Gonzalo <ErSGonzalo@Gmail.com>
2015-12-29 20:48:15 -0500

    Add in first set of file

---
+++ connection.js
@@ -0,0 +1,13 @@
+  // var Client = require('ftp');
+  var CONNECTION_SETTINGS = require('./settings.js') ;
+
+
+  // var c = new Client();
+  // c.on('ready', function() {
+  //     c.list(function(err, list) {
+  //         if (err) throw err;
+  //         console.dir(list);
+  //         c.end();
+  //     });
+  // });
+  console.log(CONNECTION_SETTINGS.host);
