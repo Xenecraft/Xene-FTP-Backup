66e22304b6a140230076920a900036a2f650044c
Eric Gonzalo <ErSGonzalo@Gmail.com>
2016-02-09 22:45:43 -0500

    Moved startTime declaration to inside function, should fix problem of folder name not changing when cron starts the job

--- connection.js
+++ connection.js
@@ -5,17 +5,18 @@ const fs = require('fs');
 const moment = require('moment');
 const settings = require('./settings.js');
 
-let c = new Client();
-let startTime = moment();
-
 function downloadFTPFiles() {
-	let todayString = startTime.format('YYYY[.]MM[.]DD');
-	let destinationPath = settings.COPY_PATH.destinationPath;
-	let copyPath = settings.COPY_PATH.folderName;
-	let finalDestinationPath = destinationPath + todayString;
+  //Instantiate all temporary variables for this routine
+  let c = new Client();
+  let startTime = moment();
+  let todayString = startTime.format('YYYY[.]MM[.]DD');
+  let destinationPath = settings.COPY_PATH.destinationPath;
+  let copyPath = settings.COPY_PATH.folderName;
+  let finalDestinationPath = destinationPath + todayString;
 
   console.log('----------------------');
   console.log('Starting your backup!');
+  console.log(`Your files will go into ${finalDestinationPath}`);
   console.log('----------------------');
   makeFolder(finalDestinationPath);
 
@@ -24,9 +25,9 @@ function downloadFTPFiles() {
       if (err) throw err;
       list.forEach((item) => {
         if (item.type == '-')
-          downloadFile(copyPath, item.name, finalDestinationPath);
+          downloadFile(c, copyPath, item.name, finalDestinationPath);
         else {
-          recursiveLookDown(copyPath + '/' + item.name, finalDestinationPath);
+          recursiveLookDown(c, copyPath + '/' + item.name, finalDestinationPath);
         }
       });
       c.end();
@@ -40,17 +41,42 @@ function downloadFTPFiles() {
   c.connect(settings.CONNECTION_SETTINGS);
 };
 
-function downloadFile(filePath, fileName, finalDestinationPath) {
+function downloadFile(c, filePath, fileName, finalDestinationPath) {
   let finalFile = filePath + '/' + fileName;
   makeFolder(finalDestinationPath + '\\' + filePath)
   c.get(finalFile, (err, stream) => {
     console.log('[Downloading]', finalFile);
     if (err) throw err;
-    stream.once('close', function() {   
-    	//Each file closes connection 	
+    stream.once('close', function() {
+      //Each file closes its own connection 	
     });
     stream.pipe(fs.createWriteStream(finalDestinationPath + '\\' + finalFile));
   })
+}
+
+function recursiveLookDown(c, topDirectory, finalDestinationPath) {
+  c.list(topDirectory, (err, list) => {
+    if (err) throw err;
+    list.forEach((item) => {
+      if (item.type == '-')
+        downloadFile(c, topDirectory, item.name, finalDestinationPath);
+      else
+        recursiveLookDown(c, topDirectory + '/' + item.name, finalDestinationPath);
+    });
+  })
+}
+
+function finishFTPDownload(startTime, finalDestinationPath) {
+  let endTime = moment();
+  let totalTime = endTime.diff(startTime, 'minutes');
+  let endTimeString = endTime.format('HH[:]mm[:]ss');
+
+  console.log('----------------------');
+  console.log(`The connection has closed. The time is currently ${endTimeString}.`);
+  console.log('Your backup has finished:');
+  console.log(`This operation took ${totalTime} minutes`);
+  console.log(`Your backup is located in ${finalDestinationPath}`);
+  console.log('----------------------');
 }
 
 function makeFolder(folderPath) {
@@ -64,32 +90,7 @@ function makeFolder(folderPath) {
   })
 }
 
-function recursiveLookDown(topDirectory, finalDestinationPath) {
-  c.list(topDirectory, (err, list) => {
-    if (err) throw err;
-    list.forEach((item) => {
-      if (item.type == '-')
-        downloadFile(topDirectory, item.name, finalDestinationPath);
-      else
-        recursiveLookDown(topDirectory + '/' + item.name, finalDestinationPath);
-    });
-  })
-}
-
-function finishFTPDownload(startTime, finalDestinationPath) {
-  let endTime = moment();
-  let totalTime = endTime.diff(startTime, 'minutes');
-  let endTimeString = endTime.format('HH[:]mm[:]ss');
-	console.log(`The connection has closed. The time is currently ${endTimeString}.`)
-  console.log('----------------------');
-  console.log('Your backup has finished:');
-  console.log(`This operation took ${totalTime} minutes`);
-  console.log(`Your backup is located in ${finalDestinationPath}`);
-  console.log('----------------------');
-}
-
 // Uncomment to test the FTP Download without the cronjob.
 //downloadFTPFiles();
-
 
 module.exports = downloadFTPFiles;
